1e9474bb4aa3a3997e417d370138881619f793df
unknown <an106203h@gmail.com>
2025-06-04 17:37:59 +0700

    change url be

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
     // API_BASE_URL: 'http://localhost:5555/api',
-    API_BASE_URL: 'https://antuhust.id.vn/api',
+    API_BASE_URL: 'https://be-access-control.onrender.com/api',
   };
   
 export default API_CONFIG;
