a39d1333ffe7c30257c9222cb13f03c3dd4c9b7a
unknown <an106203h@gmail.com>
2025-06-04 16:49:17 +0700

    change url backend

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,5 +1,6 @@
 const API_CONFIG = {
-    API_BASE_URL: 'http://localhost:5555/api',
+    // API_BASE_URL: 'http://localhost:5555/api',
+    API_BASE_URL: 'https://antuhust.id.vn/api',
   };
   
 export default API_CONFIG;
