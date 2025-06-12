eff6a7cc43f5093b6fb857c9c37d9bcbe89f7b78
unknown <an106203h@gmail.com>
2025-06-13 02:21:02 +0700

    redesign_user_page

--- src/pages/Staff/StaffPage.jsx
+++ src/pages/Staff/StaffPage.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import ListStaff from '../../components/Staff/ListStaff'
 import CreateStaff from '../../components/Staff/CreateStaff'
 import { withAuthorization } from '../../hoc'
 import StaffDetail from '../../components/Staff/StaffDetail'
+import RedesignedStaffList from '../../components/Staff/RedesignedStaffList'
 
 const StaffPage = () =>{
     return(
         <>
             <Routes>
-                <Route path='/' element = {<ListStaff />}/>
+                <Route path='/' element = {<RedesignedStaffList />}/>
                 <Route path='/create-staff' element = {<CreateStaff />}/>
                 <Route path='/:_id' element = {<StaffDetail/>}/>
             </Routes>
