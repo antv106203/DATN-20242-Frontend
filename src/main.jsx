be764a5c6ac4f96c0cea6a9cc71573d32ff7bd12
unknown <an106203h@gmail.com>
2025-06-13 11:38:30 +0700

    fix -layout 88888

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
      <Provider store={store}>
         <App />
-    </Provider>,
-  </StrictMode>,
+    </Provider>
+  </StrictMode>
 )
